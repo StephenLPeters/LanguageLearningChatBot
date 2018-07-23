b4b4be2b1a7b4ccfaddabce48d0f5d637d1aff49
Corinne Curcie <cocurc@microsoft.com>
2018-07-23 14:08:15 -0700

    Recreated projects

--- LanguageLearningChatBot/Program.cs
+++ LanguageLearningChatBot/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LanguageLearningChatBot
 {
@@ -6,7 +10,6 @@ namespace LanguageLearningChatBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
