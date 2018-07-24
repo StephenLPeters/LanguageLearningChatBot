c1ef57db6be8f7673ae4674590e62f540e1be71f
drewod93 <odonnell.drew@gmail.com>
2018-07-23 17:13:22 -0700

    dummy response

--- LanguageLearningChatBotCore/Views/Console/ConsoleView.cs
+++ LanguageLearningChatBotCore/Views/Console/ConsoleView.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 // NOTE: Install the Newtonsoft.Json NuGet package.
@@ -15,22 +16,37 @@ namespace TranslatorTextQuickStart
         {
             instance(primLang, secondLang, scenario);
             LanguageLearningChatBotCore.ResponseAnalysis analysis = respond("");
-
-            Console.ReadLine();
+            Console.WriteLine(analysis.Prompt.m_secondaryText);
+            string response = Console.ReadLine();
+            Console.WriteLine(response);
         }
         static void instance(string primLang, string secondLang, string scenario){
 
         }
-        static void respond(string userResponse){
+        static LanguageLearningChatBotCore.ResponseAnalysis respond(string userResponse){
 
             LanguageLearningChatBotCore.TranslationData prompt = new LanguageLearningChatBotCore.TranslationData();
-            prompt.m_originalLanguage = LanguageLearningChatBotCore.Language.French;
-            prompt.OriginalText = "Bonjour, ca va?";
-            prompt.
+            prompt.m_secondaryLanguage = LanguageLearningChatBotCore.Language.French;
+            prompt.m_secondaryText = "Bonjour, ca va?";
+            prompt.m_primaryLanguage = LanguageLearningChatBotCore.Language.English;
+            prompt.m_primaryText = "Hello, how are you?";
 
+            LanguageLearningChatBotCore.TranslationData response = new LanguageLearningChatBotCore.TranslationData();
+            response.m_secondaryLanguage = LanguageLearningChatBotCore.Language.French;
+            response.m_secondaryText = "Tres brien, merci";
+            response.m_primaryLanguage = LanguageLearningChatBotCore.Language.English;
+            response.m_primaryText = "Very well, thank you";
 
+            LanguageLearningChatBotCore.Correction correction = new LanguageLearningChatBotCore.Correction();
+            correction.Original = "hi";
+            correction.Offset = 1;
+            correction.Corrected = "bingo bango";
 
+            List<LanguageLearningChatBotCore.Correction> corrections = new List<LanguageLearningChatBotCore.Correction>();
+            corrections.Add(correction);
+            LanguageLearningChatBotCore.ResponseAnalysis responseAnalysis = new LanguageLearningChatBotCore.ResponseAnalysis(prompt, response, corrections);
 
+            return responseAnalysis;
         }
     }
 
