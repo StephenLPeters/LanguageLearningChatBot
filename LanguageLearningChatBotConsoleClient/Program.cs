7abf4e5033ed5a25add8ebf9d4809452f864e2b2
drewod93 <odonnell.drew@gmail.com>
2018-07-24 16:19:23 -0700

    updated program
    displays corrections

--- LanguageLearningChatBotConsoleClient/Program.cs
+++ LanguageLearningChatBotConsoleClient/Program.cs
@@ -23,15 +23,18 @@ namespace LanguageLearningChatBotConsoleClient
                 Console.WriteLine("What language would you like to chat in? <fr, es, it, en>");
                 string lang = Console.ReadLine();
                 Console.WriteLine("Waiting for server...");
+
+
                 var getControllerIDTask = getControllerID("en", lang);
                 getControllerIDTask.Wait();
                 controllerID = getControllerIDTask.Result;
 
 
-                string userResponse = "";
+                string userResponse = "1";
                 var respondTask = Respond(userResponse);
                 respondTask.Wait();
                 var serverResponse = respondTask.Result;
+                Console.WriteLine(serverResponse.Prompt.SecondaryText);
 
                 while (true)
                 {
@@ -39,16 +42,14 @@ namespace LanguageLearningChatBotConsoleClient
                     if (userResponse.Equals("?"))
                     {
                         showTranslations(serverResponse);
-
                     }
                     else
                     {
                         respondTask = Respond(userResponse);
                         respondTask.Wait();
                         serverResponse = respondTask.Result;
-                        //Console.WriteLine(serverResponse.Prompt.SecondaryText);
-                        //Console.WriteLine(serverResponse.Response.SecondaryText);
                         writeCorrections(userResponse, serverResponse);
+                        Console.WriteLine(serverResponse.Prompt.SecondaryText);
                     }
 
                 }
@@ -62,17 +63,23 @@ namespace LanguageLearningChatBotConsoleClient
             private static void writeCorrections (string response, LanguageLearningChatBotCore.ResponseAnalysis analysis)
             {
                 int resIndex = 0;
+
+                foreach (LanguageLearningChatBotCore.Correction correction in analysis.Corrections)
+                {
+                    Console.WriteLine(correction.Corrected + " " + correction.Offset);
+                }
                 foreach (LanguageLearningChatBotCore.Correction correction in analysis.Corrections)
                 {
                     if (correction.Offset > 0)
                     {
-                        Console.Write(response.Substring(resIndex, correction.Offset - 1));
+                        Console.Write(response.Substring(resIndex, correction.Offset - resIndex));
                     }
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.Write(correction.Corrected);
                     Console.ResetColor();
 
                     resIndex = correction.Offset + correction.Original.Length;
+
                 }
                 if (resIndex < response.Length - 1)
                 {
